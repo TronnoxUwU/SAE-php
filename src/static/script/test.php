98f2375b0de096dd8ac5d4733578a482bb6a7baf
TronnoxUwU <baptisterichard98@free.fr>
2025-02-03 15:10:24 +0100

    test

--- src/static/script/test.php
+++ src/static/script/test.php
@@ -1,11 +1,13 @@
 <?php
 
-$host = '@dab065e6-321b-4b4a-a173-b1dff4e871d5.supabase.co';
-$dbname = 'public';
-$user = 'dab065e6-321b-4b4a-a173-b1dff4e871d5';
-$password = 'test';
-$port = '5432'; // Par défaut, 5432
+$host = 'aws-0-eu-west-3.pooler.supabase.com';
+$dbname = 'postgres';
+$user = 'postgres.vicnhizlpnnchlerpqtr';
+$password = 'SAEMLF2025.';
+$port = '6543'; // Par défaut, 5432
 
 // Création de la connexion avec PDO
 $dsn = "pgsql:host=$host;port=$port;dbname=$dbname;user=$user;password=$password";
 $pdo = new PDO($dsn);
+
+echo"yeah";
